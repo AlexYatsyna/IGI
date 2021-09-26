b7732517085d9391ca3dc5f8670e35cf33e3eb66
Alex Yatsyna <yatsyna.firebalde@mail.ru>
2021-09-26 04:37:45 +0300

    1 lab

---
+++ WEB_953506_YATSYNA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB_953506_YATSYNA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
