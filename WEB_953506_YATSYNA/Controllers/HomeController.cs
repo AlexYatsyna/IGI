d9026b5918c93f535f009b01211825d36baf28fc
Alex Yatsyna <yatsyna.firebalde@mail.ru>
2021-09-26 16:08:39 +0300

    lab2

--- WEB_953506_YATSYNA/Controllers/HomeController.cs
+++ WEB_953506_YATSYNA/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,24 +10,43 @@ using WEB_953506_YATSYNA.Models;
 
 namespace WEB_953506_YATSYNA.Controllers
 {
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
+
     public class HomeController : Controller
     {
+        private List<ListDemo> _listDemo;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
+            _listDemo = new List<ListDemo>
+            {
+            new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+            new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+            new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] =new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
